6a9ad3b3d2a6c0f2050fe6795d89a113cf6ee152
1102137110 <s1102137110@gm.kuas.edu.tw>
2016-03-03 15:03:03 +0800

    hihicommit

---
+++ Project2/Class2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    class Class2
+    {
+        public static void test() {
+            System.Console.WriteLine("hihi");
+            System.Console.ReadLine();
+        }
+
+    }
+}
